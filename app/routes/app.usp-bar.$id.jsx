1372cf76bd89918cb3071d83d72c2544ddad0e60
yashsolanki12 <yash@ecodesoft.com>
2026-03-10 09:51:18 +0530

    Make the form & color settings page responsive also tooltip placement change

--- app/routes/app.usp-bar.$id.jsx
+++ app/routes/app.usp-bar.$id.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import { authenticate } from "../shopify.server";
 import { useParams } from "react-router";
 import UspBarForm from "../components/usp-bar/usp-bar-form";
-
-export const loader = async ({ request, params }) => {
-  await authenticate.admin(request);
-  return null;
-};
 
 export default function EditUspBar() {
   const { id } = useParams();
 
   return (
-    <Box sx={{ p: 4 }}>
+    <Box sx={{ p: { xs: 2, sm: 3, md: 4 } }}>
       <UspBarForm id={id} heading="Edit USP Bar" />
     </Box>
   );
